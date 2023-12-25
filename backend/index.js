700530e50b3fdef9433aa29c7f17347db5e6b4d0
Yashasvi <yashasvigarg14@gmail.com>
2023-12-25 13:11:06 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
-
+import { fileURLToPath } from "url"; // Import the fileURLToPath function
+import { dirname } from "path";
+import path from "path";
+ 
 const app = express();
+
+ 
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+ 
+const __filename = fileURLToPath(import.meta.url); // Use fileURLToPath to get the filename
+const _dirname = dirname(_filename); // Use dirname to get the directory name
+ 
+app.use(express.static(path.join(__dirname, "./Frontend")));
+ 
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Frontend/index.html"));
+});
+
+
 const DB = "mongodb+srv://yashasvi:yashasvi@cluster0.p463vaq.mongodb.net/qoala"
 
 mongoose.connect(DB,{
